804b6862f79c2c4eaab0adc3bacf1f42a66dbb80
Ellen <ddhkxxhxx@hotmail.com>
2024-07-21 14:04:08 +0800

    add env & bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 
 
 function App() {
-
+  useEffect(()=> {
+    //console.log(import.meta.env.VITE_API_URL, import.meta.env.VITE_API_PATH)
+    
+    (async () => {
+      const res = await axios.get(`/v2/api/${import.meta.env.VITE_API_PATH}/products/all`)
+      console.log(res)
+    })()
+  }, [])
 
   return (
-    <div>
+    <div className='App'>
+      <button type="button" className="btn btn-primary">Primary</button>
+      <button type="button" className="btn btn-secondary">Secondary</button>
+      <button type="button" className="btn btn-success">Success</button>
+      <button type="button" className="btn btn-danger">Danger</button>
+      <button type="button" className="btn btn-warning">Warning</button>
+      <button type="button" className="btn btn-info">Info</button>
+      <button type="button" className="btn btn-light">Light</button>
+      <button type="button" className="btn btn-dark">Dark</button>
+
+      <button type="button" className="btn btn-link">Link</button>
 
     </div>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './stylesheets/all.scss'
 import App from './App.jsx'
+import axios from 'axios'
+
+axios.defaults.baseURL = import.meta.env.VITE_API_URL
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
