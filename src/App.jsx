c702d2c4a9ba41c457960cbc81eac1aaca845692
Ellen <ddhkxxhxx@hotmail.com>
2024-07-21 15:11:00 +0800

    add login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,16 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+import { Routes, Route } from 'react-router-dom'
 
+import Login from './pages/Login'
 
 
 function App() {
-  useEffect(()=> {
-    //console.log(import.meta.env.VITE_API_URL, import.meta.env.VITE_API_PATH)
-    
-    (async () => {
-      const res = await axios.get(`/v2/api/${import.meta.env.VITE_API_PATH}/products/all`)
-      console.log(res)
-    })()
-  }, [])
+  
 
   return (
     <div className='App'>
-      <button type="button" className="btn btn-primary">Primary</button>
-      <button type="button" className="btn btn-secondary">Secondary</button>
-      <button type="button" className="btn btn-success">Success</button>
-      <button type="button" className="btn btn-danger">Danger</button>
-      <button type="button" className="btn btn-warning">Warning</button>
-      <button type="button" className="btn btn-info">Info</button>
-      <button type="button" className="btn btn-light">Light</button>
-      <button type="button" className="btn btn-dark">Dark</button>
-
-      <button type="button" className="btn btn-link">Link</button>
+      <Routes>
+        <Route path="/login" element={<Login/>}></Route>
+      </Routes>
 
     </div>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client'
 import './stylesheets/all.scss'
 import App from './App.jsx'
 import axios from 'axios'
+import { HashRouter } from 'react-router-dom'
 
 axios.defaults.baseURL = import.meta.env.VITE_API_URL
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
+    
   </React.StrictMode>,
 )
