d06e1da419cb055eb36b5c17a9c1b22b52a7f540
Ellen <ddhkxxhxx@hotmail.com>
2024-07-22 14:32:15 +0800

    add alert message

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,23 +6,30 @@ import { useNavigate } from "react-router-dom"
 function Login() {
   const navigate = useNavigate()
   const [data, setData] = useState({
-    username:'',
-    password:''
+    username: '',
+    password: ''
   })
+  const [loginState, setLoginState] = useState({})
+
   const handleChange = (e) => {
     const { name, value } = e.target
-    setData({...data, [ name ]: value})
+    setData({ ...data, [name]: value })
   }
-  
+
   const submit = async(e) => {
-    const res = await axios.post('/v2/admin/signin', data)
-    const { token, expired } = res.data
-    console.log(res.data)
-    document.cookie =`hexToken=${token}; expires=${new Date(expired)}`
-    //儲存token
-    //確認登入成功後進行轉址
-    if(res.data.success){
-      navigate('/admin/products')
+    try {
+      const res = await axios.post('/v2/admin/signin', data)
+      const { token, expired } = res.data
+      console.log(res.data)
+      document.cookie =`hexToken=${token}; expires=${new Date(expired)}`
+      //儲存token
+      //確認登入成功後進行轉址
+      if(res.data.success){
+        navigate('/admin/products')
+      }
+    } catch (error) {
+      console.log(error)
+      setLoginState(error.response.data)
     }
   }
 
@@ -32,8 +39,8 @@ function Login() {
       <div className="col-md-6">
         <h2>登入帳號</h2>
 
-        <div className="alert alert-danger" role="alert">
-          錯誤訊息
+        <div className={`alert alert-danger ${loginState.message? 'd-block' : 'd-none'}`} role="alert">
+          {loginState.message}
         </div>
         <div className="mb-2">
           <label htmlFor="email" className="form-label w-100">
