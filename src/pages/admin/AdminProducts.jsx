39112c145c9fc99d37664367a6925f33226c9455
Ellen <ddhkxxhxx@hotmail.com>
2024-07-24 17:41:50 +0800

    add pagination function

--- src/pages/admin/AdminProducts.jsx
+++ src/pages/admin/AdminProducts.jsx
@@ -4,6 +4,7 @@ import axios from "axios";
 
 import ProductModal from "../../components/ProductModal";
 import DeleteModal from '../../components/DeleteModal'
+import Pagination from "../../components/Pagination";
 
 function AdminProducts(){
   const [products, setProducts] = useState([])
@@ -25,8 +26,8 @@ function AdminProducts(){
     getProducts()
   }, [])
 
-  const getProducts = async() => {
-    const productRes = await axios.get(`/v2/api/${import.meta.env.VITE_API_PATH}/admin/products`)
+  const getProducts = async(page=1) => {
+    const productRes = await axios.get(`/v2/api/${import.meta.env.VITE_API_PATH}/admin/products?page=${page}`)
     console.log(productRes)
     setProducts(productRes.data.products)
     setPagination(productRes.data.pagination)
@@ -129,35 +130,9 @@ function AdminProducts(){
         
       </tbody>
     </table>
-
-    <nav aria-label="Page navigation example">
-      <ul className="pagination">
-        <li className="page-item">
-          <a className="page-link disabled" href="/" aria-label="Previous">
-            <span aria-hidden="true">&laquo;</span>
-          </a>
-        </li>
-        {
-          [...new Array(5)].map((_, i) => (
-            // eslint-disable-next-line react/no-array-index-key
-            <li className="page-item" key={`${i}_page`}>
-              <a
-                className={`page-link ${(i + 1 === 1) && 'active'}`}
-                href="/"
-              >
-                {i + 1}
-              </a>
-
-            </li>
-          ))
-        }
-        <li className="page-item">
-          <a className="page-link" href="/" aria-label="Next">
-            <span aria-hidden="true">&raquo;</span>
-          </a>
-        </li>
-      </ul>
-    </nav>
+    
+    <Pagination pagination={pagination} changePage={getProducts}/>
+    
   </div>
     
   )
