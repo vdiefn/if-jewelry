b06474103a4350bcaff4b0e2e0a761b6c8044cae
Ellen <ddhkxxhxx@hotmail.com>
2024-07-25 12:31:54 +0800

    feat: add coupon page and creact, delete, edit function

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { useEffect } from 'react'
 import Login from './pages/Login'
 import Dashboard from './pages/admin/Dashboard'
 import AdminProducts from './pages/admin/AdminProducts'
-
+import AdminCoupons from './pages/admin/AdminCoupons'
 
 function App() {
   
@@ -14,6 +14,7 @@ function App() {
         <Route path="/login" element={<Login/>}></Route>
         <Route path='/admin' element={<Dashboard />}>
           <Route path='products' element={<AdminProducts />}></Route>
+          <Route path='coupons' element={<AdminCoupons />}></Route>
         </Route>
       </Routes>
 
